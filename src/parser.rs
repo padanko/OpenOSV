e6fb025c4c6d17288bd707ecf34e368b316ef5c7
Honzawa yusei <Gadai114514810@outlook.jp>
2024-10-25 11:24:00 +0900

    random機能追加

--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,15 @@
 use regex::Regex;
+use rand::{self, Rng};
 use crate::struct_defines::Thread;
 
 fn is_decimal(s: &str) -> bool {
     s.chars().all(char::is_numeric)
 }
+
+fn randint(a: i32, b:i32) -> i32 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(a..b)
+} 
 // BAN機能「aku」のコマンドを解析する
 pub fn ban_parse(mut text: String, mut thread: Thread) -> (Thread, String) {
     let ban_command_pattern: Regex = Regex::new(r"!aku:([0-9]+)").unwrap();  // Open2chと同じ !aku コマンド
@@ -33,11 +39,19 @@ pub fn ban_parse(mut text: String, mut thread: Thread) -> (Thread, String) {
     return results;
 }
 
+// すべての「!random」をランダムな数字に置き換える
+pub fn random_parse(mut text: String) -> String { 
+    for _ in 0..text.as_str().matches("!random").count() {
+        let replaced_str = format!("<b style='color-4'>{}</b>", randint(0, 100).to_string());
+        text = text.replacen("!random", replaced_str.as_str(), 1)
+    }
+    text
+}
 
-pub fn parse_commands(text: String, thread: Thread) -> (Thread, String){
+pub fn parse_commands(mut text: String, thread: Thread) -> (Thread, String){
 
+    text = random_parse(text);
     let (thread, text) = ban_parse(text, thread);
-
     let results: (Thread, String) = (thread, text);
     return results;
 
